e080b253a7e3bfb819fe0124dded1a557d1d3fb1
Ola <ola.pabian@interia.com>
2023-12-20 16:38:13 +0100

    edit

--- src/main/java/com/example/DataCollector/DataCollectorApplication.java
+++ src/main/java/com/example/DataCollector/DataCollectorApplication.java
@@ -14,30 +14,50 @@ public class DataCollectorApplication {
 	public static void main(String[] args) throws IOException {
 		SpringApplication.run(DataCollectorApplication.class, args);
 		//PROVIDERS
-		URL url = new URL("https://www.filmweb.pl/api/v1/vod/providers/list");
-		HttpsCollector providerCollector = new HttpsCollector(url);
-		providerCollector.Collect();
-		providerCollector.writeToConsole();
+//		URL url = new URL("https://www.filmweb.pl/api/v1/vod/providers/list");
+//		HttpsCollector providerCollector = new HttpsCollector(url);
+//		providerCollector.Collect();
+//		providerCollector.writeToConsole();
 
 		//INFO (na razie Napoleon)
-		URL url1 = new URL("https://www.filmweb.pl/api/v1/film/864985/info");
-		HttpsCollector infoCollector = new HttpsCollector(url1);
-		infoCollector.Collect();
-		infoCollector.writeToConsole();
+
+		for (int filmId = 1; filmId <= 1_000_000; filmId++) {
+			try {
+				URL url = new URL("https://www.filmweb.pl/api/v1/film/" + filmId + "/info");
+				HttpsCollector infoCollector = new HttpsCollector(url);
+				infoCollector.Collect();
+				infoCollector.writeToConsole();
+			} catch (Exception e) {
+				// Handle exceptions or continue to the next film ID
+				continue;
+			}
 
 		//DESCRIPTION (na razie Napoleon)
-		URL url2 = new URL("https://www.filmweb.pl/api/v1/film/864985/description");
-		HttpsCollector descriptionCollector = new HttpsCollector(url2);
-		descriptionCollector.Collect();
-		descriptionCollector.writeToConsole();
+//		URL url2 = new URL("https://www.filmweb.pl/api/v1/film/864985/description");
+//		HttpsCollector descriptionCollector = new HttpsCollector(url2);
+//		descriptionCollector.Collect();
+//		descriptionCollector.writeToConsole();
+//
+//		//Daty premiery (na razie Napoleon)
+//		URL url3 = new URL("https://www.filmweb.pl/api/v1/film/864985/dates");
+//		HttpsCollector datesCollector = new HttpsCollector(url3);
+//		datesCollector.Collect();
+//		datesCollector.writeToConsole();
+//
+//		//Id krytyków którzy ocenili ten film (na razie Napoleon)
+//		URL url4 = new URL("https://www.filmweb.pl/api/v1/film/864985/critics ");
+//		HttpsCollector criticsIDCollector = new HttpsCollector(url4);
+//		criticsIDCollector.Collect();
+//		criticsIDCollector.writeToConsole();
+//
+//		//Wszyscy krytycy
+//		URL url5 = new URL("https://www.filmweb.pl/api/v1/film/critics");
+//		HttpsCollector criticsCollector = new HttpsCollector(url5);
+//		criticsCollector.Collect();
+//		criticsCollector.writeToConsole();
 
-		//Daty premiery (na razie Napoleon)
-		URL url3 = new URL("https://www.filmweb.pl/api/v1/film/864985/dates");
-		HttpsCollector datesCollector = new HttpsCollector(url3);
-		datesCollector.Collect();
-		datesCollector.writeToConsole();
 	}
-}
+}}
 
 
 
---
+++ src/main/java/com/example/DataCollector/Model/Person.java
@@ -0,0 +1,4 @@
+package com.example.DataCollector.Model;
+
+public class Person {
+}
